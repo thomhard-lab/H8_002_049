681bb498b8668a03c91faf55b5734882642124f9
kensein <huseinnurrahmat@gmail.com>
2020-12-14 23:08:29 +0700

    Training Day 1

---
+++ day1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 20:08:19 2020
+
+@author: Kensein
+Training Python Day 1
+"""
+
+import math as m
+
+text = "Kelas Python"
+print(text)
+text2 = "BMKG"
+print(text + " " + text2)
+
+a = 3
+b = 4
+c = a ** b
+print(c)
+
+print(text + " " + str(c))
+
+text3 = text + " " + text2 + " " + str(a + b + c)
+print(text3)
+
+d = 16
+e = m.sqrt(d)
+print(e)
+
+f = int(input())
+print(e + f)
